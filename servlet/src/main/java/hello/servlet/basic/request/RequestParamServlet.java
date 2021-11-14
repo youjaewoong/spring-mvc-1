8a276ddbd7637b5089c34ff06d12b265c3dd389a
gos1004 <nnv711@gmail.com>
2021-11-14 21:12:50 +0900

    HTTP 요청 데이터 - POST HTML For

--- servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -10,10 +10,12 @@ import java.io.IOException;
 import java.util.Enumeration;
 
 /**
- * 1. 파라미터 전송 기능
- * http://localhost:8080/request-param?username=hello&age=20
- *
- */
+* 1. 파라미터 전송 기능
+* http://localhost:8080/request-param?username=hello&age=20
+* <p>
+* 2. 동일한 파라미터 전송 가능
+* http://localhost:8080/request-param?username=hello&username=kim&age=20
+*/
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
 
