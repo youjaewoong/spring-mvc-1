f313acd1cb3e13b28df87b969f88fd5584e0291b
gos1004 <nnv711@gmail.com>
2021-11-14 16:20:59 +0900

    HttpServletRequest - 기본 사용법

--- servlet/src/main/java/hello/servlet/ServletApplication.java
+++ servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -7,9 +7,8 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @ServletComponentScan
 @SpringBootApplication
 public class ServletApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
-
 }
