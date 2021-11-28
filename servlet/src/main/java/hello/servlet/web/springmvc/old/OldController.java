fa0979f4fc6f80f77825b0efb74bbea8f3177f48
gos1004 <nnv711@gmail.com>
2021-11-28 21:59:45 +0900

    뷰 리졸버

--- servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -13,7 +13,6 @@ public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
-        //return new ModelAndView("new-form");
+        return new ModelAndView("new-form");
     }
 }
